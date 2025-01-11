59666f6c5b911649a92298f9c9f734bb037804d7
zahra-hajji-vinci <zahra.hajji@student.vinci.be>
2025-01-11 22:24:35 +0100

    Les bases du React

--- tutorials/components/src/App.tsx
+++ tutorials/components/src/App.tsx
@@ -1,35 +1,102 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import sound from "./assets/sounds/Infecticide-11-Pizza-Spinoza.mp3";
+import logo from "./assets/images/js-logo.png";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+interface HeaderProps {
+  title: string;
+  version:number;
 }
 
-export default App
+const Header = (props: HeaderProps) => {
+  return (
+    <header>
+      <h1 className="animate__animated animate__bounce">{props.title}</h1>
+      <h4>Version: {props.version}</h4>
+    </header>
+  );
+};
+
+const Main = () => {
+  return (
+    <main>
+      <p>My HomePage</p>
+      <p>
+        Because we love JS, you can also click on the header to stop / start the
+        music ; )
+      </p>
+
+      <audio id="audioPlayer" controls autoPlay>
+        <source src={sound} type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
+    </main>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer>
+      <h1 className="animate__animated animate__bounce animate__delay-2s">
+        But we also love JS
+      </h1>
+      <img src={logo} alt="" />
+    </footer>
+  );
+};
+
+function App() {
+  return (
+    <div className="page">
+      <Header title="We love Pizza" version={0+1}/>
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+{/*3 composantes React
+  Pour rappel, pour définir un type en TS, 
+  il y a deux principales façons de le faire. 
+  Soit à l'aide de type, soit à l'aide de interface.
+  Dans ce cours, nous avons choisi d'utiliser interface pour définir la structure d'objets.*/}
+
+
+/* sans type
+const Header = () => {
+  return (
+    <header>
+      <h1 className="animate__animated animate__bounce">{}</h1>
+    </header>
+  );
+}; */
+
+/*  le type de retour en JSX.Element
+const Header = (props: HeaderProps): JSX.Element => {
+  return (
+    <header>
+      <h1 className="animate__animated animate__bounce">{props.title}</h1>
+    </header>
+  );
+};
+ */
+
+/* inline prop type annotation pour typer props
+const Header = (props: {title:string}) => {
+  return (
+    <header>
+      <h1 className="animate__animated animate__bounce">{props.title}</h1>
+    </header>
+  );
+};
+*/
