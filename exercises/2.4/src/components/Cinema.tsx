131469e5743e31f86516979a69988de8be8f7a6f
zahra-hajji-vinci <zahra.hajji@student.vinci.be>
2025-01-12 22:02:50 +0100

    Update Cinema components types

---
+++ exercises/2.4/src/components/Cinema.tsx
@@ -0,0 +1,21 @@
+import { Movie } from "../types";
+
+interface CinemaProps {
+  name: string;
+  movies: Movie[];
+}
+
+const Cinema = (props: CinemaProps) => (
+  <div>
+    <h2>{props.name}</h2>
+    <ul>
+      {props.movies.map((movie) => (
+        <li key={movie.title}>
+          <strong>{movie.title}</strong> - Réalisateur : {movie.director}
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+export default Cinema;
