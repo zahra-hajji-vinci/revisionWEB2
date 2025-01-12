cf121a7c7a1ea00d82d84893d7fde8e6382e593c
zahra-hajji-vinci <zahra.hajji@student.vinci.be>
2025-01-13 00:57:39 +0100

    State multi

--- tutorials/start-state/src/components/Header/index.tsx
+++ tutorials/start-state/src/components/Header/index.tsx
@@ -1,24 +1,31 @@
 // Code: Header component
 
-import './Header.css';
-import { useState } from 'react';
+import "./Header.css";
+import { useState } from "react";
 
 interface HeaderProps {
-    title: string;
-    version:number;
-  }
-  
-  const Header = ({ title, version }: HeaderProps) => {
-    const [menuPrinted, setMenuPrinted] = useState(false);
-    return (
-      <header onClick={() => setMenuPrinted(!menuPrinted)}>
-        <h1 className="animate__animated animate__bounce">{title}</h1>
-        {menuPrinted ? `${title}... and rarely do we hate it!` : title}
-        <h4>Version: {version}</h4>
-      </header>
-    );
+  title: string;
+  version: number;
+}
+
+const Header = ({ title, version }: HeaderProps) => {
+  const [menuPrinted, setMenuPrinted] = useState(false);
+
+  const handleClick = () => {
+    console.log(`value of menuPrinted before click: ${menuPrinted}`);
+    setMenuPrinted(!menuPrinted);
   };
 
-  //destructuring assignment
+  return (
+    <header onClick={handleClick}>
+      <h1 className="animate__animated animate__bounce">
+        {menuPrinted ? `${title}... and rarely do we hate it!` : title}
+      </h1>
+      <h4>Version: {version}</h4>
+    </header>
+  );
+};
 
-  export default Header;
+//destructuring assignment
+
+export default Header;
