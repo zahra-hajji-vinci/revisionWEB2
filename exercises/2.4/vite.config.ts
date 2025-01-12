c972bb8f6182b5dc59bcf26b911c16e06cd5ee69
zahra-hajji-vinci <zahra.hajji@student.vinci.be>
2025-01-12 21:52:39 +0100

    Initial commit

---
+++ exercises/2.4/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import checker from 'vite-plugin-checker'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(),
+  checker({
+    // e.g. use TypeScript check
+    typescript: true,
+  }),
+  ],
+})
+
+
+/*npm i vite-plugin-checker -D */
+/* shift alt F pour formater le code */
