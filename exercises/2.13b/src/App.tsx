203be88f15ab4eb1a40105f90d2ee646ec8eff71
zahra-hajji-vinci <zahra.hajji@student.vinci.be>
2025-01-13 20:09:54 +0100

    Chien Compo url + refresh

--- exercises/2.13b/src/App.tsx
+++ exercises/2.13b/src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import RandomDog from "./RandomDog";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [refresh, setRefresh] = useState(false);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{ display: "flex", flexDirection: "row", gap: 10 }}>
+        <RandomDog key={`${refresh}1`} />
+        <RandomDog key={`${refresh}2`} />
+        <RandomDog key={`${refresh}3`} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <button
+        onClick={() => setRefresh(!refresh)}
+        style={{
+          marginTop: "20px",
+          padding: "10px 20px",
+          fontSize: "1em",
+          cursor: "pointer",
+        }}
+      >
+        Refresh Dogs
+      </button>
+    </>
+  );
+};
+
+export default App;
---
+++ exercises/2.13b/src/RandomDog.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+interface Dog {
+  message: string;
+  status: string;
+}
+  
+  const RandomDog = () => {
+    const [dog, setDog] = useState<Dog | undefined>(undefined);
+  
+    const fetchDogImage = () => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            setDog({
+              message: data.message ?? "No dog found",
+              status: data.status ?? "Error",
+            });
+          });
+      };
+
+    useEffect(() => {
+        fetchDogImage();
+      }, []);
+    
+  
+    if (!dog) {
+      return <p>Loading...</p>;
+    }
+    
+    return (
+        <div>
+          <h3>Random dog</h3>
+          <img src={dog.message} alt="Random dog" style={{ maxHeight: 300 }} />
+        </div>
+      );
+    };
+  
+  export default RandomDog;
+
+  
