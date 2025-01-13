0e3d8c6b72b5daba3bd468b7a375821e2ee52225
zahra-hajji-vinci <zahra.hajji@student.vinci.be>
2025-01-13 01:52:48 +0100

    ClickCounter message et compo destructuringversion Zahra

--- exercises/2.5/src/App.tsx
+++ exercises/2.5/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <ClickCounter />
+        <ClickCounter title="hey" message="You are a master in the art of clicking!" />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
--- exercises/2.5/src/components/ClickCounter.tsx
+++ exercises/2.5/src/components/ClickCounter.tsx
@@ -1,18 +1,24 @@
 import React, { useState } from 'react';
 
-function ClickCounter() {
-    const [count, setCount] = useState(0);
-  
-    return (
-      <>
-        <div className="card">
-          <button onClick={() => setCount((count: number) => count + 1)}>
-            count is {count}
-          </button>
-        </div>
-      </>
-    );
+interface ClickCounterProps {
+  title: string;
+  message: string;
+}
+
+function ClickCounter({ title, message }: ClickCounterProps) {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <div className="card">
+        <button onClick={() => setCount((count: number) => count + 1)}>
+          count is {count}
+        </button>
+        {count >= 10 && <p>{message}</p>}
+      </div>
+    </>
+  );
 }
 
 export default ClickCounter;
-  
