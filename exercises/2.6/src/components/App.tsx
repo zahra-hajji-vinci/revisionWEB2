9c1699ee6030d19b0a9737b268c93c0e8180b9cb
zahra-hajji-vinci <zahra.hajji@student.vinci.be>
2025-01-13 01:57:13 +0100

    Initial commit

---
+++ exercises/2.6/src/components/App.tsx
@@ -0,0 +1,77 @@
+import { Movie } from "../types";
+import Cinema from "./Cinema";
+import PageTitle from "./PageTitle";
+import Header from "./Header";
+import Footer from "./Footer";
+
+
+
+
+
+const App = () => {
+  const pageTitle = "Informations sur les films dans les cinémas";
+
+  const cinema1Name = "UGC DeBrouckère";
+
+  const moviesCinema1: Movie[] = [
+    {
+      title: "HAIKYU-THE DUMPSTER BATTLE",
+      director: "Susumu Mitsunaka",
+    },
+    {
+      title: "GOODBYE JULIA",
+      director: "Mohamed Kordofani",
+    },
+    {
+      title: "INCEPTION",
+      director: "Christopher Nolan",
+    },
+    {
+      title: "PARASITE",
+      director: "Bong Joon-ho",
+    },
+  ];
+
+  const cinema2Name = "UGC Toison d'Or";
+
+  const moviesCinema2: Movie[] = [
+    {
+      title: "THE WATCHERS",
+      director: "Ishana Night Shyamalan",
+    },
+    {
+      title: "BAD BOYS: RIDE OR DIE",
+      director: "Adil El Arbi, Bilall Fallah",
+    },
+    {
+      title: "TENET",
+      director: "Christopher Nolan",
+    },
+    {
+      title: "THE IRISHMAN",
+      director: "Martin Scorsese",
+    },
+  ];
+
+  return (
+    <div>
+      <Header urlLogo="https://media.istockphoto.com/id/1429764305/fr/vectoriel/bande-de-film-vierge-isol%C3%A9e-sur-le-fond-blanc.jpg?s=1024x1024&w=is&k=20&c=is5Y6cun0NC8PxJd51p4YnUoLUpyb758Bdigh4Bqn48=">
+        <h1>Tous sur les films</h1>
+      </Header>
+
+      <main className="page-content"></main>
+
+      <PageTitle title={pageTitle} />
+
+      <Cinema name={cinema1Name} movies={moviesCinema1} />
+
+      <Cinema name={cinema2Name} movies={moviesCinema2} />
+       
+      <Footer urlLogo="https://media.istockphoto.com/id/1202770152/fr/photo/bobine-de-film-disolement-sur-le-fond-jaune-lumineux-dans-les-couleurs-pastel.jpg?s=1024x1024&w=is&k=20&c=2yKBrC8oyimPdW-5IxFWN_zxFPVK3KWYL9OE2gVmVX4=">
+        <p>© 2021 UGC Cinémas</p>
+      </Footer>
+    </div>
+  );
+};
+
+export default App;
