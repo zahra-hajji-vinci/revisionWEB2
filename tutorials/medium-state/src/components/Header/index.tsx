1d15e38d3c78283c950a20e3b97bbdf1eefe81e5
zahra-hajji-vinci <zahra.hajji@student.vinci.be>
2025-01-13 02:40:49 +0100

    Initial commit

---
+++ tutorials/medium-state/src/components/Header/index.tsx
@@ -0,0 +1,31 @@
+// Code: Header component
+
+import "./Header.css";
+import { useState } from "react";
+
+interface HeaderProps {
+  title: string;
+  version: number;
+}
+
+const Header = ({ title, version }: HeaderProps) => {
+  const [menuPrinted, setMenuPrinted] = useState(false);
+
+  const handleClick = () => {
+    console.log(`value of menuPrinted before click: ${menuPrinted}`);
+    setMenuPrinted(!menuPrinted);
+  };
+
+  return (
+    <header onClick={handleClick}>
+      <h1 className="animate__animated animate__bounce">
+        {menuPrinted ? `${title}... and rarely do we hate it!` : title}
+      </h1>
+      <h4>Version: {version}</h4>
+    </header>
+  );
+};
+
+//destructuring assignment
+
+export default Header;
